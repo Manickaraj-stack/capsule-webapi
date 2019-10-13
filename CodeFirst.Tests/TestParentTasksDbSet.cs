e6d7785d13860541a6c87b4cff6e694ffc40685b
manickaraj-stack <manickaraj.p@cognizant.com>
2019-10-14 04:16:46 +0530

    capsule web api changes

---
+++ CodeFirst.Tests/TestParentTasksDbSet.cs
@@ -0,0 +1,17 @@
+﻿using CodeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst.Tests
+{
+    class TestParentTasksDbSet : TestDbSet<ParentTask>
+    {
+        public override ParentTask Find(params object[] keyValues)
+        {
+            return this.SingleOrDefault(task => task.Parent_Id == (int)keyValues.Single());
+        }
+    }
+}
